04173de7c93ad9a783c085d9dba37af0bc6d42e6
Netra Amrale <30404609+neetra@users.noreply.github.com>
2022-05-12 14:59:42 -0700

    Update HomeActivity.java

--- app/src/main/java/com/example/blogapplication/HomeActivity.java
+++ app/src/main/java/com/example/blogapplication/HomeActivity.java
@@ -34,8 +34,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getUser();
-//        DBHandler db = new DBHandler(getApplicationContext());
-//        db.createTable();
+        DBHandler db = new DBHandler(getApplicationContext());
+        db.createTable();
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
